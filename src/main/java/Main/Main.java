b7f28f1dc1e9ffcf36e3c9ecf6f9a3d88e3ae631
Takezo9 <midden.py@gmail.com>
2024-08-17 19:15:51 -0300

    Feature Conta Banco concluida

--- src/main/java/Main/Main.java
+++ src/main/java/Main/Main.java
@@ -1,7 +1,23 @@
 package Main;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner rd = new Scanner(System.in);
+        System.out.println("Por favor, digite o seu nome: ");
+        String nome_cliente = rd.nextLine();
+        System.out.println("Por favor, digite a Agência");
+        String agencia = rd.nextLine();
+        System.out.println("Por favor, digite o número da Agência: ");
+        int num_agencia = Integer.parseInt(rd.nextLine());
+        System.out.println("Por favor, digite o saldo");
+        double saldo = Double.parseDouble(rd.nextLine());
+
+        System.out.println(STR."Olá \{nome_cliente}, " +
+                STR."obrigado por criar uma conta em nosso banco, sua agência é \{agencia},"
+        + " conta " + num_agencia + " e seu saldo " + saldo +
+                " já está disponível para saque.");
+
     }
 }
